7598faed511006bacff0ae615ae0fce420c40019
Tom Dorian <tom.dorian@eiratech.com>
2024-01-11 11:54:13 +0000

    Making Rviz launch alongisde the fake slave node

--- launch/can_open_simulation.launch.py
+++ launch/can_open_simulation.launch.py
@@ -11,7 +11,46 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     package_name = "ros2_turtlebot_can_open"
+    xacro_file = PathJoinSubstitution(
+        [FindPackageShare(package_name), "urdf", "turtlebot3_burger.urdf.xacro"]
+    )
 
+    robot_description = Command(
+        [PathJoinSubstitution([FindExecutable(name='xacro')]), ' ', xacro_file]
+    )
+
+    rviz_file = PathJoinSubstitution(
+        [FindPackageShare(package_name), "launch", "basic.rviz"]
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description}
+        ],
+    )
+    
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        parameters=[
+            {'robot_description': robot_description},
+        ],
+    )
+
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_file],
+        parameters=[
+            {'robot_description': robot_description},
+        ]
+    )
     slave_config_n5 = PathJoinSubstitution(
         [FindPackageShare(package_name), "config/Turtlebot_PoistionControl", "cia402_slave_n5.eds"]
     )
@@ -45,6 +84,9 @@ def generate_launch_description():
     nodes_to_start = [
         slave_node_1,
         slave_node_2,
+        rviz2,
+        joint_state_publisher_node,
+        robot_state_publisher,
     ]
 
     return LaunchDescription(nodes_to_start)
